f7bea08c772e5bb323701d0441336721e087aea1
Jindřich Bobek <bobekji1@uhk.cz>
2025-05-03 01:05:25 +0300

    Code format

--- src/core/dns.ts
+++ src/core/dns.ts
@@ -1,27 +1,27 @@
 import { DomainValidationResult } from "../types";
 
 export async function validateDomain(domain: string): Promise<DomainValidationResult | null> {
-  if (typeof window !== "undefined") {
-    return null;
-  }
-
-  const dns = await import('dns').then(mod => mod.promises);
-
-  let hasMxRecords = false;
-  let hasValidDomain = false;
-
-  try {
-    const mxRecords = await dns.resolveMx(domain);
-    hasMxRecords = mxRecords.length > 0;
-    hasValidDomain = true;
-  } catch {
-    try {
-      await dns.resolve(domain);
-      hasValidDomain = true;
-    } catch {
-      hasValidDomain = false;
+    if (typeof window !== "undefined") {
+        return null;
     }
-  }
 
-  return { hasValidDomain, hasMxRecords };
+    const dns = await import('dns').then(mod => mod.promises);
+
+    let hasMxRecords = false;
+    let hasValidDomain = false;
+
+    try {
+        const mxRecords = await dns.resolveMx(domain);
+        hasMxRecords = mxRecords.length > 0;
+        hasValidDomain = true;
+    } catch {
+        try {
+            await dns.resolve(domain);
+            hasValidDomain = true;
+        } catch {
+            hasValidDomain = false;
+        }
+    }
+
+    return { hasValidDomain, hasMxRecords };
 }
